b21b082142c969d055f3dea191d429d0d2829926
huanhoahongso3-collab <huanhoahongso3@gmail.com>
2025-09-14 15:19:37 +0700

    fix bug
    fix bug gravity in THL Main Block, thus impact on performance on client

--- src/main/java/mc/dhp/foss/k7a2fools/block/THLMainBlockBlock.java
+++ src/main/java/mc/dhp/foss/k7a2fools/block/THLMainBlockBlock.java
@@ -5,11 +5,11 @@ import org.checkerframework.checker.units.qual.s;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.core.BlockPos;
 
-public class THLMainBlockBlock extends FallingBlock {
+public class THLMainBlockBlock extends Block {
 	public THLMainBlockBlock() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.GRAVEL).strength(10f, 20f).lightLevel(s -> 5).speedFactor(1.1f).jumpFactor(1.1f));
 	}
